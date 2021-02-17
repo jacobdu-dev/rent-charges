0b01d426e7d7d3140a0ce470e4739e1cdc097f67
jacobdu-dev <64336220+jacobdu-dev@users.noreply.github.com>
2021-02-17 13:06:49 -0800

    Mac Build

---
+++ venmo_key.py
@@ -0,0 +1,5 @@
+from venmo_api import Client
+username = str(input("Please enter your venmo username and press enter: "))
+password = str(input("Please enter your venmo password and press enter: "))
+access_token = Client.get_access_token(username=username, password=password)
+print("Your Access Token is ", access_token + ". Ensure this is used when creating a new profile!")
